59f7a6b5b4b4ac1ed0df16158c37070e75b0ac03
Justin Barber <jbarber@altalang.com>
2011-04-14 09:10:29 -0400

    Fill in missing hours with 0 counts

--- gateway_rpt_utils.py
+++ gateway_rpt_utils.py
@@ -9,10 +9,10 @@ Version .01
 
 import os, os.path, re, types
 
-def get_daily_reports(path):
+def get_reports(path):
     """
     Pass a path to directory where reports are kept for parsing.
-    Returns a fully qualified path to files if only a relative path is passed.
+    Returns a fully qualified path to files.
     """
 
     files = os.listdir(path)
@@ -21,21 +21,73 @@ def get_daily_reports(path):
         abspath_files.append(os.path.abspath('%s\\%s'%(path,f)))
     return abspath_files
 
-def formGatewayCommsByTime(gateway, data_tuples_list):
+def is_consecutive_hour(first_hour,second_hour):
+    """
+    Determines if the first_hour is followed by the second hour.
+    """
+    if first_hour > 24 or second_hour > 24:
+        raise Exception("Hours must be with in 24 Hour Range")
+    if first_hour == 24 and second_hour == 00:
+        return True
+    if second_hour == (first_hour + 1):
+        return True
+    return False
+
+def fill_in_hours(gateway,date,start_hour,next_hour):
+    missing = []
+    for i in range(int(start_hour),int(next_hour)):
+        missing.append( (date,'%02d'%i,gateway,0,0) )
+    return missing
+
+def check_for_missing_hours(hour_list,gateway,date,next_hour):
+    """
+    Returns Tuples for missing hours filling in the provided gateway and 0 for counts
+    """
+
+    missing = []
+
+    # We have an empty hour_list and the first hour to add isn't 0
+    if not hour_list and int(next_hour) != 0:
+        return fill_in_hours(gateway,date,0,next_hour)
+    else:
+        # Okay we have a list need to add missing hours
+        if int(next_hour) != 0:
+            if hour_list[0][0] == date:
+                return fill_in_hours(gateway,date,hour_list[0][1],next_hour)
+            else:
+                # New Day. Lets make sure we finished off prior day
+                if hour_list[0][1] != 23:
+                    missing +=  fill_in_hours(gateway,date,hour_list[0][1],24)
+                # Now check current day
+                if int(next_hour) != 0:
+                    missing += fill_in_hours(gateway,hour_list[0][0],0,next_hour)
+    return missing
+
+
+def formGatewayByTime(gateway, data_tuples_list,fill=True):
     """
     Tuples are immutable so we have to create a new tuple
     given the gateway and data associated with it.
 
     Format ( YYYY/MM/DD, HH, Gateway, Docs, Chars )
+    
+    If fill is passed then missing hours are added with data
+    counts of 0.
     """
-    gatewayCommsByTime = []
+    gatewayByTime = []
+
+    # Sort. Necessary if we are doing fill.
+    data_tuples_list.sort()
 
     for data_tuple in data_tuples_list:
-        gatewayCommsByTime.append( (data_tuple[0], data_tuple[1], gateway, int(data_tuple[2]), int(data_tuple[3])) )
-    return gatewayCommsByTime
+        if fill:
+            #gatewayByTime += fill_in_hours(gatewayByTime,gateway,data_tuple[0],data_tuple[1])
+            gatewayByTime.extend(check_for_missing_hours(gatewayByTime,gateway,data_tuple[0],data_tuple[1]))
+        gatewayByTime.append( (data_tuple[0], data_tuple[1], gateway, int(data_tuple[2]), int(data_tuple[3])) )
+    return gatewayByTime
     
 
-def parse_file_for_comm_data(rpt):
+def parse_file_for_data(rpt):
     """
     Return a list of tuples:
     [(Date in format YYYY/MM/DD, HH24, DocCount, CharacterCount), ...]
@@ -48,25 +100,25 @@ def parse_file_for_comm_data(rpt):
     # automatically split out for us.
     with open(rpt,"r") as f:
         data = f.read()
-    commsByTime = re.findall("(?P<date>[0-9]{4}/[0-9]{2}/[0-9]{2}) (?P<hh>[0-9]{2}),(?P<docs>[0-9]+),(?P<chars>[0-9]+)\n",data)
+    dataByTime = re.findall("(?P<date>[0-9]{4}/[0-9]{2}/[0-9]{2}) (?P<hh>[0-9]{2}),(?P<docs>[0-9]+),(?P<chars>[0-9]+)\n",data)
 
     # We need to get the gateway this data is for.
     # Assumed each file only has data for one gateway. 
     gateway = re.findall("DATE \(YYYY/MM/DD HH24\),(?P<gateway>[a-zA-Z0-9 ]+) Docs,chars\n",data)[0]
 
     # Create new tuple with gateway included
-    return formGatewayCommsByTime(gateway, commsByTime)
+    return formGatewayByTime(gateway, dataByTime)
 
-def munge_all_data(path="sample_sql"):
+def parse_files(path="sample_sql"):
     """
     Create a report that doesn't care about the protocol.
     It will show statistics (either/both doc or characters) by day and hour.
     """
     data = []
 
-    reports = get_daily_reports(path)
+    reports = get_reports(path)
     for report in reports:
-        data.extend(parse_file_for_comm_data(report))
+        data.extend(parse_file_for_data(report))
 
     # Exclude duplicates lazily by turning it into a set
     # and then back into a list.
@@ -120,7 +172,7 @@ def write_report(data,rptFormat='CSV',name=None):
         rpt_data = write_dictionary(data)
         rpt.write(rpt_data)
 
-def create_report(data=munge_all_data,docs=True,chars=True,gateway=True,rptFormat='CSV'):
+def munge_data(data=parse_files,docs=True,chars=True,gateway=True,fill=False):
     """
     Takes the raw data and creates a report.
     A callable can be passed in leu of the data to generate data.
@@ -140,6 +192,7 @@ def create_report(data=munge_all_data,docs=True,chars=True,gateway=True,rptForma
     # If gateway=False then the Hours in the Day only point to Doc/Char counts.
     dates = {}
 
+    # need to update using setdefault
     for piece in data:
         if not dates.has_key(piece[0]):
             if gateway:
@@ -187,8 +240,7 @@ def create_report(data=munge_all_data,docs=True,chars=True,gateway=True,rptForma
                         Hour['Chars'] += piece[4]
                     if docs:
                         Hour['Docs'] += piece[3]
-    
-    write_report(dates,rptFormat)
+
     return dates
 
 def test_function(*args):
@@ -198,6 +250,7 @@ def test_function(*args):
         print arg
         
 if __name__ == "__main__":
-    #masticated_mess = munge_all_data("sample_sql") 
-    #masticated_mess = munge_all_data() 
-    create_report(gateway=False,docs=False)
+    #masticated_mess = parse_files("sample_sql") 
+    #masticated_mess = parse_files() 
+    dates = munge_data(gateway=False,docs=False)
+    write_report(dates)
